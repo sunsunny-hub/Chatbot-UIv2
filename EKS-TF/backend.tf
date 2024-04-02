2125488a8ec3122885d9c51ee8fbb88766346bf7
sunsunny-hub <surajsingh5233@gmail.com>
2024-04-02 17:17:28 +0530

    updated s3 bucket name and added dynamodb table

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tetris-bucket"
+    bucket         = "chatbot-ui-v2"
     region         = "us-east-1"
     key            = "Chatbot-UI/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
