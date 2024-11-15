610384fff8e2ade7110a045d097b3e0df42ad624
Durgaprasad228 <mallahdurgaprasad@gmail.com>
2024-11-15 18:40:30 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "chatbot-ui-v2"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     key            = "Chatbot-UIv2/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
